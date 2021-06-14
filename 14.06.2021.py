f626304bf538e669f29613b1ffcdf741cd2a4daf
Alla <sviatt1313@gmail.com>
2021-06-14 19:50:54 +0300

    instrument

--- 14.06.2021.py
+++ 14.06.2021.py
@@ -1,22 +1,18 @@
 zakaz = []
 for number in range(10):
-      print('Список доступных марок ноутбуков в продаже:\n 1. Lenovo,\n 2. Asus\n 3. Apple\n 4. HP')
-      notebook_mark = ['Lenovo','Asus', 'Apple', 'HP']
-      print('Введите номер ноутбука, который вы хотите приобрести')
+      print('Список доступных марок инструментов в продаже:\n 1. Casio,\n 2. Yamaha\n 3. Roland\n 4. Tesler')
+      musical_instrument = ['Casio','Yamaha', 'Roland', 'Tesler']
+      print('Введите номер производителя инструмента, который вы хотите приобрести')
       mark = int(input())
-      print('Список доступных диагоналей экранов:\n-15.6\n-14.0\n-13.3\n-17.3\n-14.1')
-      print('Введите диагональ экрана ноутбука')
-      diag_display_nb = input()
-      print('Список доступных процессоров:\n 1. Core-i5\n 2. Core-i7\n 3. Ryzen 5\n 4. Core-i3\n 5. Pentium Gold')
-      proc_nb = ['Core-i5', 'Core-i7', 'Ryzen 5', 'Core-i3', 'Pentium Gold']
-      print('Введите номер выбранного процессора:')
-      proc = int(input())
-      print('Выберите и введите объем оперативной памяти: от 4 гб до 32 гб')
-      ram_nb = input()
-      print('Выберите номер модели видеокарты из списка:\n 1. Intel\n 2. NVIDIA\n 3. AMD\n 4. Apple')
-      gc_nb = ['Intel', 'NVIDIA', 'AMD', 'Apple']
-      gc = int(input())
-      number = " Модель ноутбука: " + notebook_mark[mark-1] + "; Диагональ экрана: " + diag_display_nb + "; Процессор: "+ proc_nb[proc-1] + "; Объем оперативной памяти: " + ram_nb + "; Модель видеокарты: " + gc_nb[gc-1]
+      print('Тип инструмента:\ 1.Кейтар ,\n 2.Синтезато\n 3.Рабочая станция, \n 4.Цифровое пианино')
+      kind_instrument = ['Кейтар','Синтезатор', 'Рабочая станция', 'Цифровое пианино']
+      kind = int(input())
+      print('Выберите количество клавиш: от 13 до 88')
+      keys = input()
+      print('Выберите цвет из списка:\n 1. Черный\n 2. Белый\n 3. Бежевый\n 4. Серый')
+      color_instr = ['черный', 'белый', 'бежевый', 'серый']
+      color = int(input())
+      number = " Модель инструмента: " + musical_instrument[mark-1] + "; Тип инструмента: " + kind_instrument[kind-1] + "; Количество клавиш: " + keys + "; Цвет: " + color_instr[color-1]
       zakaz.append(number)
       print('Хотите продолжить покупки? (да/нет)')
       qst = input()
@@ -25,13 +21,3 @@ for number in range(10):
 print('Ваш заказ:')
 print('\n'.join(zakaz))
 print("Спасибо за то, что выбрали наш магазин,\nчерез 10 минут с вами свяжется наш менеджер,\nдля дальнейшего оформления заказа")
-
-#print('Ваш заказ\n'
-#      + '\n-----------------------------'
-#      + "\nМодель ноутбука: " + notebook_mark[n-1]
-#     + "\nДиагональ экрана: " + diag_display_nb
-#    + "\nПроцессор: "+ proc_nb[proc-1]
-#      + "\nОбъем оперативной памяти: " + ram_nb
-#      + "\nМодель видеокарты: " + gc_nb[gc-1]
-#      + "\n-----------------------------"
-#     )
